bcd81923f8f45f17d4dbb8c8aa0352a8d5de909f
mradul dwivedi <mraduldwivedi@gmail.com>
2021-07-27 23:08:19 +0530

    Initial Commit.

---
+++ IPLDashBoard/src/test/java/com/mradul/IplDashBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mradul;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IplDashBoardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
